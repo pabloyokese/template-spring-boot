3e7e6539dda6feb6afca2000be1f0663aa7774fa
pabloyokese <jpo_54@hotmail.com>
2016-05-22 09:25:58 -0400

    addding dandelion to load resources

--- src/main/java/com/jp/youplace/ConfigurationApp.java
+++ src/main/java/com/jp/youplace/ConfigurationApp.java
@@ -2,6 +2,7 @@ package com.jp.youplace;
 
 import org.h2.server.web.WebServlet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,15 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+import com.github.dandelion.core.web.DandelionFilter;
+import com.github.dandelion.core.web.DandelionServlet;
+import com.github.dandelion.thymeleaf.dialect.DandelionDialect;
 
 @Configuration
 @EnableWebSecurity
@@ -47,4 +57,28 @@ public class ConfigurationApp extends WebSecurityConfigurerAdapter {
 	    registration.addUrlMappings("/console/*");
 	    return registration;
 	}
+	
+	@Bean
+	public DandelionFilter dandelionFilter(){
+		return new DandelionFilter();
+	}
+	
+	@Bean
+	public DandelionDialect dandelionDialect(){
+		return new DandelionDialect();
+	}
+	
+	@Bean
+	public ServletRegistrationBean dandelionServletRegistrationBean(){
+		return new ServletRegistrationBean(new DandelionServlet(),"/dandelion-assets/*");
+	}
+	
+	@Bean
+	public FilterRegistrationBean filterRegistrationBean(){
+		FilterRegistrationBean bean = new FilterRegistrationBean();
+		bean.setFilter(dandelionFilter());
+		return bean;
+	}
+
+	
 }
